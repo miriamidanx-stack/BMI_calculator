ac6c552653ffb6c882e1edff566defb7383866fe
Miriamidanx-stack <miriamidanx@gmail.com>
2026-07-29 10:59:10 -0700

    first commit

---
+++ BMI_calculator_Daniel.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+root =Tk()
+root.title("BMI Calculator")
+root.geometry("470x580+300+200")
+root.resizable(False,False)
+root.configure(bg="#f0f1f5")
+
+
+
+root.mainloop()
